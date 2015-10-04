f64949f4f7cc8c0065eb80e85337fa48e5abde29
Martin Marenz <marenz@itp.uni-leipzig.de>
2015-10-04 23:00:02 +0200

    bugfix in addlogwise

--- src/addlogwise.hpp
+++ src/addlogwise.hpp
@@ -10,20 +10,20 @@ constexpr T log_zero() {
 
 // TODO: maybe use __builtin_expect since a > b in almost all cases
 // TODO: can be optimized if std::exp(x) = 0, return value don't need log1p(exp())
-//template <class T>
-//inline double addlogwise(T a, T b) noexcept {
-//  return (a > b) ? (a + std::log1p(std::exp(b - a)))
-//                 : (b + std::log1p(std::exp(a - b)));
-//}
-
 template <class T>
-inline T addlogwise(T a, T b) noexcept {
-  if (a < b){
-    std::swap(a, b);
-  }
-  if (b <= -1000.0){
-    return a;
-  }else{
-    return a + std::log1p(std::exp(b-a));
-  }
+inline double addlogwise(T a, T b) noexcept {
+  return (a > b) ? (a + std::log1p(std::exp(b - a)))
+                 : (b + std::log1p(std::exp(a - b)));
 }
+
+//template <class T>
+//inline T addlogwise(T a, T b) noexcept {
+//  if (a < b){
+//    std::swap(a, b);
+//  }
+//  if (b-a <= -1000.0){
+//    return a;
+//  }else{
+//    return a + std::log1p(std::exp(b-a));
+//  }
+//}
