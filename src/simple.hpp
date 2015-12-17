97392a0a999026a5e9fe6717c3183c6ce8642885
Martin Marenz <marenz@itp.uni-leipzig.de>
2015-12-17 15:45:21 +0100

    add doxygen for simple.hpp

--- src/simple.hpp
+++ src/simple.hpp
@@ -3,20 +3,34 @@
 #include <algorithm>
 #include <tuple>
 
+/*-----------------------------------------------------------------------------
+ *  this file contains some standard statistical functions
+ *-----------------------------------------------------------------------------*/
 namespace consus
 {
 
+/// calculates the mean of Data
+///
+/// just a convinient call to std::accumulate
+/// \tparam T   - container with begin() and end()
+/// \param Data - container with something convertable to a floating point number
 template<class T>
 double mean(const T& Data){
   return std::accumulate(Data.begin(), Data.end(), 0.0);
 }
  
-/*-----------------------------------------------------------------------------
- *  min, max, mean value, variance, naive error, binning error
- *-----------------------------------------------------------------------------*/
+/// calculates the minimum/maximum/mean value/variance/naive error/binning errr
+/// of Data
+///
+/// ignores numbers which are not finite (NaN/inf/etc)
+///
+/// \tparam Data - container with random access and size() method
+/// \param Data - container with something convertable to a floating point number
+/// \param NumBins - number of bins used to calculate binning error
+/// \result - tuple with 6 elements (min, max, mean, variance, naive error, binning error)
 template <class T>
 std::tuple<double, double, double, double, double, double> simple_stats(
-    const T& Data, const unsigned int NumBins) {
+    const T& Data, const size_t NumBins) {
   assert(NumBins <= Data.size());
   size_t BinWidth = Data.size() / NumBins;
   double min = std::numeric_limits<double>::max();
@@ -25,24 +39,22 @@ std::tuple<double, double, double, double, double, double> simple_stats(
   size_t j, i;
   double binmean;
   double num = 0;
-  {
-    for (i = 0; i < NumBins; ++i) {
-      binmean = 0.0;
-      unsigned nums = 0;
-      for (j = 0; j < BinWidth; ++j) {
-        if (std::isfinite(Data[j + i * BinWidth])) {
-          ++nums;
-          min = std::min(Data[j + i * BinWidth], min);
-          max = std::max(Data[j + i * BinWidth], max);
-          mean += Data[j + i * BinWidth];
-          variance += Data[j + i * BinWidth] * Data[j + i * BinWidth];
-          binmean += Data[j + i * BinWidth];
-        }
+  for (i = 0; i < NumBins; ++i) {
+    binmean = 0.0;
+    unsigned nums = 0;
+    for (j = 0; j < BinWidth; ++j) {
+      if (std::isfinite(Data[j + i * BinWidth])) {
+        ++nums;
+        min = std::min(Data[j + i * BinWidth], min);
+        max = std::max(Data[j + i * BinWidth], max);
+        mean += Data[j + i * BinWidth];
+        variance += Data[j + i * BinWidth] * Data[j + i * BinWidth];
+        binmean += Data[j + i * BinWidth];
       }
-      num += nums;
-      binmean /= nums;
-      binerr += binmean * binmean;
     }
+    num += nums;
+    binmean /= nums;
+    binerr += binmean * binmean;
   }
   mean /= num;
   variance -= num * mean * mean;
@@ -52,6 +64,6 @@ std::tuple<double, double, double, double, double, double> simple_stats(
   binerr /= static_cast<double>(NumBins + 1.0);
   binerr = std::sqrt(binerr / (static_cast<double>(NumBins)));
   return std::make_tuple(min, max, mean, variance, naiveerror, binerr);
-} 
+}
 
 } /* end of namespace consus */ 
